a7a5b53ae2697a4e7ff5c0ae3aed33fa668158ae
Даня <45875910+danyatochkaru@users.noreply.github.com>
2022-11-15 16:14:03 +0300

    Заглушки

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,26 @@
+import Head from "next/head";
+
 export default function Home() {
 	return (
-		<section style={{ textAlign: "center" }}>
+		<section
+			style={{
+				height: "100vh",
+				display: "flex",
+				alignItems: "center",
+				justifyContent: "flex-end",
+				padding: 20,
+				flexDirection: "column",
+			}}
+		>
+			<Head>
+				<title>Piybeep Manager</title>
+				<meta name="description" content="Piybeep Manager Panel" />
+				<link rel="icon" href="/favicon.ico" />
+			</Head>
 			<samp>[В разарботке]</samp>
+			<a href="https://piybeep.com" target="_blank">
+				piybeep
+			</a>
 		</section>
 	);
 }
